46acd34e241332f7ecbc1e420a2927daaba87bd0
Anthony Mansur <amansur99@gmail.com>
2021-10-23 12:48:16 -0400

    Init fps project

---
+++ Source/TimeWarp/TimeWarp.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class TimeWarp : ModuleRules
+{
+	public TimeWarp(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
