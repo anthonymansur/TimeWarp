cf21e0ad54a1b76677e904c0d0df5d1a8875c841
Anthony Mansur <amansur99@gmail.com>
2021-10-30 20:47:35 -0400

    Fixing possession bug

--- Source/TimeWarp/TimeWarpPlayerController.cpp
+++ Source/TimeWarp/TimeWarpPlayerController.cpp
@@ -13,3 +13,20 @@ void ATimeWarpPlayerController::ClientPostLogin(APlayerController* NewPlayer)
 	static_cast<ATimeWarpGameMode*>(
 		UGameplayStatics::GetGameMode(GetWorld()))->RespawnPlayerEvent(NewPlayer);
 }
+
+// From https://answers.unrealengine.com/questions/124855/what-can-change-the-rotation-of-a-newly-possessed.html 
+void ATimeWarpPlayerController::ForceControlRotation(const FRotator& NewRotation)
+{
+    if (!HasAuthority())
+    {
+        // Just call SetControlRotation() on the client
+        SetControlRotation(NewRotation);
+        return;
+    }
+
+    // On server
+    SetControlRotation(NewRotation);
+
+    // Force on client
+    ClientSetRotation(NewRotation);
+}
